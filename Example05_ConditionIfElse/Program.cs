4529849db4bd235f7fc2805d4c2cf1d3e0fd9c2a
Alexandr Kopytov <kopytov.schura2014@yandex.ru>
2023-11-20 22:38:25 +0500

    Example 05 Condition_If_Else

---
+++ Example05_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "tom")
+{
+    Console.WriteLine("Да это же Tom!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+
